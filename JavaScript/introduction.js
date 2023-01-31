fde45c1cdad5d13228e8f9f40628b38a6f53bd5b
Tshepo1000 <tshepotubatsi@gmail.com>
2023-01-31 20:00:04 +0200

    html and css

---
+++ JavaScript/introduction.js
@@ -0,0 +1,11 @@
+function myFunc(){
+    document.getElementById("demo").innerHTML="Linking external CSS";
+}
+
+let myName = "Tshepo"
+let age = 26
+var gender = "Male"
+var married = false
+const education = "Matric"
+
+console.log(myName, gender, age, education, married)
---
+++ JavaScript/jsrev.js
@@ -0,0 +1,7 @@
+function off() {
+    document.getElementById('light').src="light-bulb-on-off-png-11553940208oq66nq8jew.png";
+}
+
+function on() {
+    document.getElementById('light').src="light-bulb-on-off-png-11553940319kdxsp3rf0i.png";
+}
